d8d7baa00259ab16deb404e086b91ed8fff55b70
jackkellerk <jjk322@lehigh.edu>
2020-06-03 12:19:38 -0400

    continuing working on the comment

--- lib/comment.dart
+++ lib/comment.dart
@@ -3,9 +3,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-// These are import methods for custom icons
-import 'icons/custom_icons_icons.dart';
-
 class Comment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,68 +10,102 @@ class Comment extends StatelessWidget {
   // This is for screen height and width
   final double width = MediaQuery.of(context).size.width;
   final double height = MediaQuery.of(context).size.height;
+  final TextStyle commentTS = new TextStyle(color: new Color(0xffe0e0e0), decoration: TextDecoration.none, fontWeight: FontWeight.bold,);
 
     return Padding(
       padding: EdgeInsets.only(bottom: 70.0),
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          new ClipOval(
-            child: new Container(
-              color: const Color(0xffc9c9c9),
-              height: 45,
-              width: 45,
-              child: new Padding(
-                padding: EdgeInsets.only(top: 1.5),
-                child: new Image(
-                  image: AssetImage('assets/images/anon.png'), // TODO: Make this dynamically loaded
-                ),
-              ),
-            ),
-          ),
-          new Container( // TODO: Make this a stack with text in front
-            width: width * 0.73,
-            height: 85,
-            color: Colors.transparent,
-            child: new Container(
-              decoration: new BoxDecoration(
-                color: const Color(0xffe0e0e0),
-                borderRadius: BorderRadius.all(Radius.circular(40.0))
-              ),
-            ),
-          ),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget> [
           new Padding(
-            padding: EdgeInsets.only(left: 5.0, right: 5.0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new Image(
-                  image: AssetImage('assets/images/upvote.png'), // 0xfff75422 is upvote color
-                  height: 25,
-                  width: 25,
-                ),
-                new Text(
-                  "0",
-                  style: new TextStyle(
-                    color: const Color(0xffe0e0e0),
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Product-Sans',
-                    decoration: TextDecoration.none,
+            padding: EdgeInsets.only(bottom: 5.0),
+            child: new Text(
+              "Sponsored",
+              style: commentTS
+            ),
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              new ClipOval(
+                child: new Container(
+                  color: const Color(0xffc9c9c9),
+                  height: 45,
+                  width: 45,
+                  child: new Padding(
+                    padding: EdgeInsets.only(top: 1.5),
+                    child: new Image(
+                      image: AssetImage('assets/images/anon.png'), // TODO: Make this dynamically loaded
+                    ),
                   ),
                 ),
-                Transform.rotate(
-                  angle: math.pi,
-                  child: new Image(
-                    image: AssetImage('assets/images/upvote.png'), // 0xfff75422 is upvote color
-                    height: 25,
-                    width: 25,
+              ),
+              new Container( // TODO: Make this a stack with text in front
+                width: width * 0.73,
+                height: 85,
+                color: Colors.transparent,
+                child: new Container(
+                  decoration: new BoxDecoration(
+                    color: const Color(0xffe0e0e0),
+                    borderRadius: BorderRadius.all(Radius.circular(40.0))
+                  ),
+                ),
+              ),
+              new Padding(
+                padding: EdgeInsets.only(left: 5.0, right: 5.0),
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    new Image(
+                      image: AssetImage('assets/images/upvote.png'), // 0xfff75422 is upvote color
+                      height: 25,
+                      width: 25,
+                    ),
+                    new Text(
+                      "0",
+                      style: new TextStyle(
+                        color: const Color(0xffe0e0e0),
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Product-Sans',
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    Transform.rotate(
+                      angle: math.pi,
+                      child: new Image(
+                        image: AssetImage('assets/images/upvote.png'), // 0xfff75422 is upvote color
+                        height: 25,
+                        width: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          new Padding(
+            padding: EdgeInsets.only(top: 5.0),
+            child: new Row(
+              children: <Widget>[
+                new Padding(
+                  padding: EdgeInsets.only(left: 85.0),
+                  child: new Text(
+                    "Just now!",
+                    style: commentTS
+                  ),
+                ),
+                new Padding(
+                  padding: EdgeInsets.only(left: 85.0),
+                  child: new Text(
+                    "0 replies",
+                    style: commentTS
                   ),
                 ),
               ],
             ),
           ),
-        ],
+        ]
       ),
     );
   }
